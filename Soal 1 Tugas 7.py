9eb9c5fee62c8f64f0404c2f331863130cca243a
muhammadfagih <80554853+muhammadfagih@users.noreply.github.com>
2021-04-17 04:36:29 +0700

    Add files via upload

---
+++ Soal 1 Tugas 7.py
@@ -0,0 +1,17 @@
+a = input('Masukkan string\n>> ')
+
+#kapitalisasi
+b = a.capitalize()
+print(b)
+
+#count
+c = a.count(a)
+print('jumlah string ada', c)
+
+#besar
+d = a.upper()
+print(d)
+
+# kecil
+e = a.lower()
+print(e)
